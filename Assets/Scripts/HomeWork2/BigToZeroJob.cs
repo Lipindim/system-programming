3b6be6f714d54b261e9839564e9dacd7b66b0445
lipindim <lipindim@gmail.com>
2022-01-14 19:14:16 +1000

    Домашняя работа 2

---
+++ Assets/Scripts/HomeWork2/BigToZeroJob.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+
+
+namespace HomeWork2
+{
+
+    public struct BigToZeroJob : IJob
+    {
+        public NativeArray<int> Numbers;
+        public int MaxValue;
+
+        public void Execute()
+        {
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                if (MaxValue <= Numbers[i])
+                    Numbers[i] = 0;
+            }
+        }
+    }
+
+}
+
---
+++ Assets/Scripts/HomeWork2/RotationsJob.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Jobs;
+
+
+namespace HomeWork2
+{
+    public struct RotationsJob : IJobParallelForTransform
+    {
+        public Vector3 Rotation;
+
+        public void Execute(int index, TransformAccess transform)
+        {
+            transform.rotation = Quaternion.AngleAxis(Rotation.x, Vector3.right) * transform.rotation;
+            transform.rotation = Quaternion.AngleAxis(Rotation.y, Vector3.up) * transform.rotation;
+            transform.rotation = Quaternion.AngleAxis(Rotation.z, Vector3.forward) * transform.rotation;
+        }
+    }
+}
+
