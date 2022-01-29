a8c33e5dfc9938b550e6d152d7ffbe8bbfbd4292
lipindim <lipindim@gmail.com>
2022-01-29 19:01:22 +1000

    Домашняя работа 5

---
+++ Assets/Scripts/HomeWork5/Player/PlayerSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace HomeWork5
+{
+    [CreateAssetMenu(menuName = "Data/PlayerSettings")]
+    public class PlayerSettings : ScriptableObject
+    {
+        public string PlayerName;
+    }
+}
---
+++ Assets/Scripts/HomeWork5/UI/InputName.cs
@@ -0,0 +1,36 @@
+﻿using Characters;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace HomeWork5
+{
+    public class InputName : MonoBehaviour
+    {
+        [SerializeField] private PlayerSettings _playerSettings;
+        [SerializeField] private InputField _nameInput;
+
+        private void Start()
+        {
+            _nameInput.onValueChanged.AddListener(OnNameChanged);
+            ShipController.Connected += OnConnected;
+        }
+
+        private void OnDestroy()
+        {
+            _nameInput.onValueChanged.RemoveAllListeners();
+            ShipController.Connected -= OnConnected;
+        }
+
+        private void OnConnected()
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void OnNameChanged(string name)
+        {
+            _playerSettings.PlayerName = name;
+        }
+    }
+}
